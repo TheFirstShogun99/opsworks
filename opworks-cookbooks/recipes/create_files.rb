039ad382ec69c4b6b78744fd0b4dc11b1d232df7
Son Pham Hoang Thanh <sonpham170199@outlook.com>
2023-02-19 19:10:44 +0700

    Update

--- opworks-cookbooks/recipes/create_files.rb
+++ opworks-cookbooks/recipes/create_files.rb
@@ -1,3 +1,4 @@
+# Create a file "index.html" directly from a task to the machine.
 file "Create a file" do
     content "<html>This is a placeholder for the home page.</html>"
     group "root"
@@ -5,7 +6,16 @@ file "Create a file" do
     owner "ec2-user"
     path "/tmp/create_file/index.html"
   end
-  
+  # Copy the "index.html" from /tmp/create_file/index.html to /tmp/copy_file/index.html
   remote_file '/tmp/copy_file/index.html' do
     source 'file:///tmp/create_file/index.html'
+  end
+
+  # Copy a file from Subdirectory "Files" in the cookbooks to another place on the machine.
+  cookbook_file "Copy a file" do  
+    group "root"
+    mode "0755"
+    owner "ec2-user"
+    path "/tmp/create-directory-demo/hello.txt"
+    source "hello.txt"  
   end
